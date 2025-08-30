15fafda68106a34eff5c64b50d5858f178ad6410
Christian <christiandave120702@gmail.com>
2025-08-30 15:23:52 +0800

    chore: minimal update

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,4 +1,5 @@
 import Badges from "../components/Badges";
+import Sidebar from "../components/Sidebar";
 import { FaHtml5 } from "react-icons/fa";
 import { FaCss3Alt } from "react-icons/fa";
 import { FaLaravel } from "react-icons/fa";
@@ -13,29 +14,31 @@ import { SiDjango } from "react-icons/si";
 
 export default function Dashboard() {
   return (
-    <div className="ml-80 fixed w-[80%] h-[94vh] bg-gray-950 rounded-2xl shadow-[0px_0px_3px_gray]">
-      <header>
-        <Badges />
-        <div className="programming-languages relative m-6 w-[97%] h-20 overflow-hidden">
-          <div className="categories flex gap-16 whitespace-nowrap">
-            {[...Array(2)].map((_, i) => (
-              <div key={i} className="flex gap-10">
-                <FaHtml5 className="text-6xl text-red-800" />
-                <FaCss3Alt className="text-6xl text-blue-800" />
-                <FaLaravel className="text-6xl text-red-800" />
-                <FaReact className="text-6xl text-sky-400" />
-                <SiTypescript className="text-6xl text-blue-600" />
-                <SiJavascript className="text-6xl text-yellow-300" />
-                <FaPython className="text-6xl text-blue-300" />
-                <SiPhp className="text-6xl text-purple-900" />
-                <FaJava className="text-6xl text-red-400" />
-                <RiNextjsFill className="text-6xl text-white rounded-full" />
-                <SiDjango className="text-6xl text-green-900" />
-              </div>
-            ))}
+    <>
+      <div className="ml-80 fixed w-[80%] h-[94vh] bg-gray-950 rounded-2xl shadow-[0px_0px_3px_gray]">
+        <header>
+          <Badges />
+          <div className="programming-languages relative m-6 w-[97%] h-20 overflow-hidden">
+            <div className="categories flex gap-16 whitespace-nowrap">
+              {[...Array(2)].map((_, i) => (
+                <div key={i} className="flex gap-10">
+                  <FaHtml5 className="text-6xl text-red-800" />
+                  <FaCss3Alt className="text-6xl text-blue-800" />
+                  <FaLaravel className="text-6xl text-red-800" />
+                  <FaReact className="text-6xl text-sky-400" />
+                  <SiTypescript className="text-6xl text-blue-600" />
+                  <SiJavascript className="text-6xl text-yellow-300" />
+                  <FaPython className="text-6xl text-blue-300" />
+                  <SiPhp className="text-6xl text-purple-900" />
+                  <FaJava className="text-6xl text-red-400" />
+                  <RiNextjsFill className="text-6xl text-white rounded-full" />
+                  <SiDjango className="text-6xl text-green-900" />
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
-      </header>
-    </div>
+        </header>
+      </div>
+    </>
   );
 }
